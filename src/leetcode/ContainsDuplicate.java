78ca5f4a86d067981bd2f53479c1db04f590346d
Sai Siddhardha Maguluri <Siddhardha.Maguluri@ou.edu>
2023-04-01 16:42:14 -0500

    add comments for contains duplicate problem.

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -7,6 +7,12 @@ public class ContainsDuplicate {
 	 public boolean containsDuplicate(int[] nums) {
 		 Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
 		 
+		 /* loop through the array and check if the current number is
+		  * already there in the hashtable or not. if it is there, get the value
+		  * associated with it(which is the count of the number`) and increment
+		  * the value by one. if it is not there, just add the number as key and 1
+		  * as value.
+		 */
 		 for(int i=0; i<nums.length; i++){
 			 if(ht.get(nums[i]) != null){
 				 ht.put(nums[i], ht.get(nums[i])+1);
@@ -14,8 +20,12 @@ public class ContainsDuplicate {
 	         else{
 	            ht.put(nums[i],1);
 	         }
-	        }
-
+	      }
+		 
+		  /* now we again loop through the hashtable, and get the value 
+		   * associated with the key, if the value is equal or more than 2,
+		   * it means we have a duplicate. so, we will return true.
+		   */
 	      for(int i=0;i<ht.size(); i++){
 	    	  if(ht.get(nums[i])>=2)
 	    		  return true;
