a4928e3a72c79b9c9c8224b38d853d0b0c7cdf73
Sai Siddhardha Maguluri <Siddhardha.Maguluri@ou.edu>
2023-04-06 09:26:51 -0500

    Array Product Except Self - O(N)

--- src/leetcode/ArrayProductExceptSelf.java
+++ src/leetcode/ArrayProductExceptSelf.java
@@ -1,49 +1,33 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.List;
-
 public class ArrayProductExceptSelf {
 	
-	/* a brute-force approach. I added all the numbers in nums array to Hashtable, with index  
-	 * as key and number as value. After that i am iteration the array, during each iteration
-	 * i am getting all the keys of a hashtable and if the key is not equal to index, then i 
-	 * am calculating the product and storing the product in a new array at that index.
-	 * 
-	 * I guess this is incorrect use of hashtable. 
+	/* Another approach is to use concept similar to prefix sum. This approach's 
+	 * time complexity is O(N).
 	 */
 	public int[] productExceptSelf(int[] nums) {
 		
-		int[] answer = new int[nums.length];
+		int[] prefix = new int[nums.length];
 		
-		Hashtable<Integer, Integer> ht = new Hashtable<>();
-		
+		int product = 1;
 		for(int i=0;i< nums.length; i++) {
-			ht.put(i, nums[i]);
+			prefix[i] = product;
+			product *= nums[i];
 		}
 		
-		
-		for(int i=0; i< nums.length; i++) {
-			int product = 1;
-			Enumeration<Integer> keys = ht.keys();
-			while (keys.hasMoreElements()) {
-	            Integer key = keys.nextElement();
-	            if(key != i) {
-	            	product *= nums[key];
-	            }
-	        }
-			answer[i] = product;
-		}    
-		
-		return answer;
+		int product1 = 1;
+		for(int j = (nums.length-1); j >=0; j--) {
+			prefix[j] = prefix[j] * product1;
+			product1 *= nums[j];
+		}
+		    
+		return prefix;
     }
 
 	public static void main(String[] args) {
 		int nums[] = new int[] {1,2,3,4};
 		ArrayProductExceptSelf ap = new ArrayProductExceptSelf();
-		System.out.println(ap.productExceptSelf(nums));
+		System.out.println(ap.productExceptSelf(nums)[0]);
 
 	}
 
